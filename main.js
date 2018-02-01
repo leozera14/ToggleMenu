162023fd9a4d7e7ddfac5f1608bb27d7ddd59f6c
Leo Faleiros <leo_faleiros@hotmail.com>
2018-02-01 19:28:15 -0200

    Fix

---
+++ main.js
@@ -0,0 +1,62 @@
+/* NAVIGATION */
+function navMobile() {
+    const el = document.querySelector('.navMobile');
+    if (el.classList.contains('navMobileOpen')) {
+        el.classList.remove('navMobileOpen');
+    } else {
+        el.classList.add('navMobileOpen');
+    }
+}
+
+/* JSON */
+function carregarDados() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "dados.json", true);
+    xhttp.send();
+}
+
+
+/* CAROUSEL */
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("Slides");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
+
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("Slides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 4500); //
+}
